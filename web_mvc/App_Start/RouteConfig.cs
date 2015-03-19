2e070b4ab1b8be05e9b50af23d788db52a9c5160
doug-e7systems <doug@e7systems.com>
2015-03-18 17:04:18 -0700

    Added web_mvc project
    and 3 views:  Home, Login and Projects

---
+++ web_mvc/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace web_mvc
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //explicitly set routes here
+            //TEMPORARY
+            routes.MapRoute(
+                name: "home",
+                url: "",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "projects",
+                url: "projects",
+                defaults: new { controller = "Projects", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "login",
+                url: "login",
+                defaults: new { controller = "Projects", action = "Index" }
+            );
+        }
+    }
+}
